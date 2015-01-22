4a4b510535982eca7886586617dbcc03e168be5a
keikha <keikha@yahoo-inc.com>
2015-01-22 14:45:04 -0800

    my fist app

---
+++ MyFirstApp/app/src/main/java/com/myinc/keikha/myfirstapp/SimpleDisplayActivity.java
@@ -0,0 +1,60 @@
+package com.myinc.keikha.myfirstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class SimpleDisplayActivity extends ActionBarActivity {
+
+     private EditText etWords; // currently null
+     private TextView tvLabel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_display);
+
+        etWords = (EditText) findViewById(R.id.etWords);
+        tvLabel = (TextView) findViewById(R.id.tvLabel);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_simple_display, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSubmit(View view) {
+        // Toast == simple alert
+
+        // get the value from the text field ( whatever was typed)
+        String fieldValue = etWords.getText().toString();
+        // set the value on the label
+
+        tvLabel.setText(fieldValue);
+        // display the value as an alert
+        Toast.makeText( this, fieldValue, Toast.LENGTH_SHORT).show();
+    }
+}
