6e3bc2ad71c4a8b524aaf53cba8f883f31ac882f
keikha <keikha@yahoo-inc.com>
2015-01-22 15:37:20 -0800

    todo layout and behaviour

--- SimpleToDo/app/src/main/java/com/myinc/keikha/simpletodo/MainActivity.java
+++ SimpleToDo/app/src/main/java/com/myinc/keikha/simpletodo/MainActivity.java
@@ -4,14 +4,29 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+    ArrayList<String> items ;
+    ArrayAdapter<String> itemAdapter;
+    ListView lvItems ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<String>();
+        itemAdapter = new ArrayAdapter<String>( this , android.R.layout.simple_list_item_1 , items );
+        lvItems = (ListView) findViewById(R.id.lvItems);
+
+
     }
 
 
